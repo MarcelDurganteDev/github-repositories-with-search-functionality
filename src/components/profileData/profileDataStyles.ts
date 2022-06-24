11f378ef26ef7b250ac7df402dd394d644f2d0be
Marcel Durgante Dev <imfo@marceldurgante.dev>
2022-06-25 00:44:05 +0200

    Left Side user details refined

--- src/components/profileData/profileDataStyles.ts
+++ src/components/profileData/profileDataStyles.ts
@@ -1,32 +1,132 @@
 import styled, { css } from 'styled-components';
 
-import { RiGroupLine, RiBuilding4Line, RiMapPinLine, RiMailLine, RiLinksLine } from 'react-icons/ri';
+import {
+  RiGroupLine,
+  RiBuilding4Line,
+  RiMapPinLine,
+  RiMailLine,
+  RiLinksLine
+} from 'react-icons/ri';
 
 export const Container = styled.div``;
 
-export const Flex = styled.div``;
+// Avatar + username (h1) + name (h2)
 
-export const Avatar = styled.img``;
+export const Flex = styled.div`
+  display: flex;
+  align-items: center;
 
-export const Row = styled.div``;
+  > div {
+    margin-left: 24px;
 
-export const Column = styled.div``;
+    > h1 {
+      font-size: 26px;
+      line-height: 1.25;
+      color: var(--gray-dark);
+      font-weight: 600;
+    }
 
-// custom styles to pass to all icons
+    > h2 {
+      font-size: 20px;
+      color: var(--username);
+      font-weight: 300;
+    }
+  }
 
-const iconCSS = css`
-width: 16px;
-height: 16px;
-fill: var(--icon);
-flex-shrink: 0;
+  @media (min-width: 768px) {
+    flex-direction: column;
+    align-items: flex-start;
+
+    > div {
+      margin-left: 0;
+      margin-top: 16px;
+    }
+  }
 `;
 
-export const PeopleIcon = styled(RiGroupLine)`${iconCSS}`;
+export const Avatar = styled.img`
+  width: 16%;
+  height: auto;
+  border-radius: 50%;
+  
 
-export const CompanyIcon = styled(RiBuilding4Line)`${iconCSS}`;
+  @media (min-width: 768px) {
+    width: 100%;
+  }
+  
+`;
 
-export const LocationIcon = styled(RiMapPinLine)`${iconCSS}`;
+// Text and icons for 'followers' + 'following'
 
-export const EmailIcon = styled(RiMailLine)`${iconCSS}`;
+export const Row = styled.ul`
+  display: flex;
+  align-items: center;
 
-export const BlogIcon = styled(RiLinksLine)`${iconCSS}`;
+  // added to make to make 'folowers' and 'following' row break when screen is too small
+  flex-wrap: wrap;
+  margin: 20px 0;
+
+  > li {
+    display: flex;
+    align-items: center;
+
+    > span {
+      font-size: 14px;
+      color: var(--gray);
+    }
+    /* '*' all internal items will have this margin */
+    > * {
+      margin-right: 5px;
+    }
+  }
+`;
+
+// company, location, email and blog
+export const Column = styled.ul`
+  li {
+    display: flex;
+    align-items: center;
+    font-size: 14px;
+  }
+// starting from the 1st 'li' element all will have 10px margin top separating them
+  li + li {
+    margin-top: 10px;
+  }
+
+  span {
+    margin-left: 5px;
+
+    // if the screen size is too small the text will break inside adding '...'
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+`;
+
+// custom styles to pass to all icons
+const iconCSS = css`
+  width: 16px;
+  height: 16px;
+  fill: var(--icon);
+  flex-shrink: 0;
+`;
+
+export const PeopleIcon = styled(RiGroupLine)`
+  ${iconCSS}
+`;
+
+export const CompanyIcon = styled(RiBuilding4Line)`
+  ${iconCSS}
+`;
+
+export const LocationIcon = styled(RiMapPinLine)`
+  ${iconCSS}
+`;
+
+export const EmailIcon = styled(RiMailLine)`
+  ${iconCSS}
+`;
+
+export const BlogIcon = styled(RiLinksLine)`
+  ${iconCSS}
+`;
--- src/pages/profile/profileStyles.ts
+++ src/pages/profile/profileStyles.ts
@@ -1,6 +1,39 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
-export const Main = styled.div``;
-export const LeftSide = styled.div``;
-export const RightSide = styled.div``;
+// most outer container
+
+export const Container = styled.div`
+--horizontalPadding: 16px;
+--verticalPadding: 24px;
+
+padding: var(--verticalPadding) var(--horizontalPadding);
+`;
+
+export const Main = styled.div`
+display: flex;
+flex-direction: column;
+margin: 0 auto;
+// same max width as in GitHub 
+max-width: 1280px;
+
+@media (min-width: 768px) {
+  flex-direction: row;
+}
+`;
+
+
+export const LeftSide = styled.div`
+
+// github division is 25% for the left side on web view and 75% for the right side 
+@media (min-width: 768px) {
+  width: 25%;
+}
+`;
+
+
+export const RightSide = styled.div`
+  @media (min-width: 768px) {
+    width: 75%;
+  }
+`;
+
