78fd16d95235252a935429ec35ef40b1504cdcb9
Marcel Durgante Dev <imfo@marceldurgante.dev>
2022-06-27 02:25:44 +0200

    Replaced static data with dynamic data in Profile.tsx
    In TabContent function and ProfileData, RepositoriesList and RepositoryCard comoponents.

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -52,7 +52,7 @@ const Profile: React.FC = () => {
         repos
       });
     });
-  }, [] );
+  }, [username] );
 
   // if data error show message to user else show profile data and repositories
   if ( data?.error ) {
@@ -67,7 +67,8 @@ const Profile: React.FC = () => {
     <div className='content'>
       <RepositoryIcon />
       <span className='label'>Repositories</span>
-      <span className='number'>81</span>
+     {/* if I don't use 'data.user?' I get undefined error. This I believe is because here I am not sure if user is loaded or not */}
+      <span className='number'>{data.user?.public_repos}</span>
     </div>
   );
 
@@ -76,15 +77,15 @@ const Profile: React.FC = () => {
       <Main>
         <LeftSide>
           <ProfileData
-            username={'MarcelDurganteDev'}
-            name={'MarcelDurganteDev'}
-            avatarUrl={'https://avatars.githubusercontent.com/u/92659392?v=4'}
-            followers={1}
-            following={2}
-            company={'Marcel Durgante Dev'}
-            location={'Barcelona, Spain'}
-            email={'info@marceldurgante.dev'}
-            blog={'https://www.linkedin.com/in/marcel-dev/'}
+            username={data.user.login}
+            name={data.user.name}
+            avatarUrl={data.user.avatar_url}
+            followers={data.user.followers}
+            following={data.user.following}
+            company={data.user.company}
+            location={data.user.location}
+            email={data.user.email}
+            blog={data.user.blog}
           />
         </LeftSide>
         <RightSide>
@@ -96,16 +97,18 @@ const Profile: React.FC = () => {
           <RepositoriesList>
             <h2>Repositories</h2>
             <div>
-              {[1, 2, 3, 4, 5, 6].map(n => (
+              {data.repos.map(repo => (
                 <RepositoryCard
-                  key={n}
-                  username={'MarcelDurganteDev'}
-                  reponame={'repo-name'}
-                  description={'repo-description'}
+                  // not sure whether is best to use name or id as key
+                  // @TODO: check this
+                  key={repo.name}
+                  username={repo.owner.login}
+                  reponame={repo.name}
+                  description={repo.description}
                   // not sure if this is the best way to do this but I do not want all to be just one lenguage. So for testing I will use this.
-                  language={n % 3 === 0 ? 'JavaScript' : 'TypeScript'}
-                  stars={1}
-                  forks={1}
+                  language={repo.language}
+                  stars={repo.stargazers_count}
+                  forks={repo.forks}
                 />
               ))}
             </div>
