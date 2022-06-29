9285f56b3699d8a6df43bbb692402367209c3b21
Marcel Durgante Dev <imfo@marceldurgante.dev>
2022-06-29 13:28:41 +0200

    deleted server file

--- server/src/app.ts
+++
@@ -1,9 +0,0 @@
-import express, {Application, Request, Response, NextFunction} from 'express';
-
-const app: Application = express();
-
-app.get( '/', ( req: Request, res: Response, next: NextFunction ) => {
-  res.send('Hello MVST!');
-});
-
-app.listen(5000, () => console.log('Server is running on port 5000'));
